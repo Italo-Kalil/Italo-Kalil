a61779e07728818f617452d69c2fa2bf9a0df135
Italo-Kalil <italofarias071@gmail.com>
2024-03-05 14:20:23 -0400

    teste de commit

---
+++ debug/index.php
@@ -0,0 +1,6 @@
+<?php 
+require_once __DIR__ .'\..\vendor\autoload.php';
+use Italo\Composerteste\Conta_bancaria;
+$conta = new Conta_bancaria();
+
+?>
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace Italo\Composerteste;
+class Conta_bancaria{
+    
+}
+$conta1 = new Conta_bancaria();
+var_dump($conta1);
+?>
